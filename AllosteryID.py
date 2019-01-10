fa5a4e09c84510f2c3f41991434979c3c5ad588d
RyanHausler <rmh1995@gmail.com>
2019-01-10 10:00:30 -0500

    Initial commit

---
+++ AllosteryID.py
@@ -0,0 +1,108 @@
+import Bio
+from Bio.PDB import *
+from Bio import AlignIO
+from Bio.Align.Applications import ClustalwCommandline
+from pypdb import *
+import re
+import scoria
+from IPython.display import HTML
+import os, sys
+import pprint
+import glob
+from GetLigand import get_ligand, align_proteins, get_seq, create_chain_molecules, get_ligand_as_strucutre
+import copy
+#-- AllosteryID
+#-- Ryan Hausler
+#Input: PDB ID of query protein
+#Output: Aligned/unaligned PDB structures of similar BLAST results
+#	 Files containing only the ligands that were bound to each BLAST result
+
+#Create a pdb list
+cwd = os.getcwd()
+pdbl = PDBList()
+
+#Ask for query protien
+protein=input("Please enter query protein: ")
+query=protein
+proteinName=cwd+'/Original_'+query+'/pdb'+protein+'.ent'
+
+#Make directory for Alignment files
+if not os.path.exists(cwd+'/Alignment_'+protein+'/'):
+    os.makedirs(cwd+'/Alignment_'+protein+'/')
+#Make directory for moved pdb files
+if not os.path.exists(cwd+'/Moved_'+query+'/'):
+    os.makedirs(cwd+'/Moved_'+query+'/')
+#Make directory for original pdb files
+if not os.path.exists(cwd+'/Original_'+query+'/'):
+    os.makedirs(cwd+'/Original_'+query+'/')
+#Make Directory for Chains
+if not os.path.exists(cwd+'/Chains_'+query+'/'):
+    os.makedirs(cwd+'/Chains_'+query+'/')
+#Make Directory for Ligands
+if not os.path.exists(cwd+'/Ligands_'+query+'/'):
+    os.makedirs(cwd+'/Ligands_'+query+'/')
+#Create parser object
+parser = PDBParser()
+
+#Obtain query protein ID
+fir=cwd+'/Original_'+query+'/'
+pdbl.retrieve_pdb_file(protein, file_format='pdb', pdir=fir)
+
+#Download structure of query protein
+template = parser.get_structure(protein, proteinName)
+
+#Runs BLASTP on pdb id
+#Uses pypdb package
+blast_results = get_blast2(protein, chain_id='A')
+
+#Parses through BLAST results
+#Obtains matched protein ID and score
+#Saves each tuple into a list
+#example [(id, score), (id, score),...]
+myList=[]
+for protein in range(len(blast_results[0])):
+	results = str(blast_results[1][protein])
+	id_start = results.find('</a>')+4
+	id_end = results.find(':', 0)
+	score_start = results.find('Score = ')+8
+	score_end = results.find(' bits', score_start)
+	tup = (results[id_start:id_end], int(float(str(results[score_start:score_end]))))
+	myList.append(tup)
+
+#Download all BLASTp matches
+for match in myList:
+    protein = match[0]
+    pdbl.retrieve_pdb_file(protein, file_format='pdb', pdir=fir)
+
+#Align only the BLAST results with a score above 250 to query protein
+#Downloads the aligned/moved proteins
+#Uses get_seq and align_proteins methods in GetLigands.py
+for match in myList:
+    if match[1] > 250:
+        #Name of ith protein on list
+        compare_protein = match[0]
+        #Get the structure of the ith protein on the list
+        moving = cwd+'/Original_'+query+'/pdb'+compare_protein.lower()+".ent"
+        #Get all ligands from molecule
+        ligand_selection = get_ligand_as_strucutre(moving)
+        print(moving)
+        print(ligand_selection)
+        #Create a chain molecule from this structure
+        chain_mol = create_chain_molecules(moving, ligand_selection)
+        i = 1
+        #Save each chain and align with the query protein
+        for chain in chain_mol:
+            name = compare_protein.lower()+str(i)
+            save_name = cwd+'/Chains_'+query+'/pdb'+name+'.pdb'
+            chain.save_pdb(save_name)
+            #Align each chain with template
+            align_proteins(template, name, query)
+            i=i+1
+
+#For every moved chain, get all of the ligands and 
+#place them in individual PDB files
+#Uses get_ligand methods in GetLigands.py
+for filename in glob.glob(cwd+'/Moved_'+query+'/'+"*1.pdb"):
+    filename2 = filename.lstrip(cwd+'/Moved_'+query+'/')
+    outputfilename = cwd+'/Ligands_'+query+'/'
+    get_ligand(filename, outputfilename+'ligand_'+filename2, proteinName)   
---
+++ GetLigand.py
@@ -0,0 +1,242 @@
+import scoria
+import Bio
+import numpy as np
+from Bio.PDB import *
+from Bio import AlignIO
+from Bio.Align.Applications import ClustalwCommandline
+from pypdb import *
+import re
+import scoria
+from IPython.display import HTML
+import os, sys
+import copy
+
+#Retrieves all ligands bound to a protein
+#Deletes any ligands that are not within 5 A of query protein
+"""
+Given the filename of a molecule, creates a file containing ligands bound to the molecule
+
+:params str filename: filename containing molecule
+
+:returns: null
+:rtype: null
+"""
+def get_ligand(filename, outputfile, query_file):
+	#Load in query protein structure
+	query = scoria.Molecule()
+	query.load_pdb_into(query_file)
+	#Load in moved protien
+	full_protein = scoria.Molecule()
+	full_protein.load_pdb_into(filename)
+	#Gets a selection of all atoms that are protein or that won't be in a good ligand
+	protein_selection = full_protein.select_atoms({'resname':["MSE", "ALA", "ARG", "ASN", "ASP", "CYS", "GLN", "GLU", 
+                "GLY", "HIS", "ILE", "LEU", "LYS", "MET", "PHE", "PRO", 
+                "SER", "THR", "TRP", "TYR", "VAL", "HOH", "WAT", "TIP", "DOD", "NAG", "EDO", "UNX", "SO4", 
+                "ZN", "CA", "GOL", "CL", "NA", "MG", "ACT", "HEM", 
+                "DC", "ZN", "FME", "DG", "DA", "CS", "DT", "CU", 
+                "SO4", "PO4", "NA", "NH2", "CVM", "BMA", "FUL", "MES", 
+                "GSH", "EPE", "F09", "IOD", 
+                "PEG", "MAN", "FAD", "ACE", "MN", "NAP", "K", "UNK", 
+                "EOH", "CD", "FUC", "FE"]})
+	#invert the Selection so we have only ligands
+	ligand_selection = full_protein.invert_selection(protein_selection)
+	#Load selection into molecule
+	ligand = full_protein.get_molecule_from_selection(ligand_selection)
+	#Save pdb file
+	ligand.save_pdb(outputfile)
+
+	#Get each individual molecule
+	#Store in list of lists (bound)
+	bound = separate_into_molecules(filename)
+
+	#For each molecule in the matched protein...
+	mol_num = 0
+	for mol in reversed(bound):
+		#As long as it's not the actual protein
+		if(mol_num != len(bound)):
+		    #Convert mol to numpy array
+		    mol_array = np.array(mol)
+		    #create molecule from this selection
+		    this_molecule = full_protein.get_molecule_from_selection(mol_array, serial_reindex=False)
+			#If the molecule is further than 5 A from the protein...
+		    if this_molecule.get_distance_to_another_molecules(query) > 5:     #NOT USING THE RIGHT PROTEIN
+			    #remove mol from selection
+			    for atom in reversed(mol):
+				    full_protein.delete_atom(atom)
+	mol = bound[0]
+	for atom in reversed(mol):
+		full_protein.delete_atom(atom)
+	full_protein.save_pdb(outputfile)
+
+#Get a list of individual ligands from a ligand selection
+def separate_into_molecules(filename):
+	#List of lists containing selections of each molecule
+	bound = []
+	#Read in each line of pdb file
+	with open(filename) as myFile:
+		content = myFile.readlines()
+	mol = []
+	atom_num = 0
+	for line in content:
+		#Check if next atom is hetatom
+		ter = line[0:3]
+		ter = str(ter)
+		if(ter == 'END'):
+			break
+		if(ter == 'TER'):
+			#Add the current molecule to bound and clear mol
+			bound.append(mol)
+			mol = []
+			atom_num = atom_num - 1
+		else:
+			mol.append(atom_num)
+		atom_num = atom_num + 1
+	return(bound)
+
+#Gets ligands from full protein
+#These ligands will later be added to each chain separately
+"""
+Given the filename of a molecule, returns a selection of all
+ligands in pdb file
+
+:params str filename: pdb file containing protein and bound ligands
+
+:returns numpy array of ligand selection 
+
+"""
+def get_ligand_as_strucutre(filename):
+	protein = scoria.Molecule()
+	protein.load_pdb_into(filename)
+	atom_inf = protein.get_atom_information()
+	#Get all atoms that are protein and selections we don't want
+	protein_selection = protein.select_atoms({'resname':["MSE", "ALA", "ARG", "ASN", "ASP", "CYS", "GLN", "GLU", 
+                "GLY", "HIS", "ILE", "LEU", "LYS", "MET", "PHE", "PRO", 
+                "SER", "THR", "TRP", "TYR", "VAL", "HOH", "WAT", "TIP", "DOD", "NAG", "EDO", "UNX", "SO4", 
+                "ZN", "CA", "GOL", "CL", "NA", "MG", "ACT", "HEM", 
+                "DC", "ZN", "FME", "DG", "DA", "CS", "DT", "CU", 
+                "SO4", "PO4", "NA", "NH2", "CVM", "BMA", "FUL", "MES", 
+                "GSH", "EPE", "F09", "IOD", 
+                "PEG", "MAN", "FAD", "ACE", "MN", "NAP", "K", "UNK", 
+                "EOH", "CD", "FUC", "FE"]})
+	#invert the Selection
+	ligand_selection = protein.invert_selection(protein_selection)
+	#ligand = protein.get_molecule_from_selection(ligand_selection)
+	return(ligand_selection)
+
+#Gets selection of each chain with ligands bound
+#Do before structural alignment
+#Questions, talk to Patrick Ropp
+"""
+Given the filename of a molecule, returns a dictionary containing all 
+chain molecules
+
+:params str filename: filename containing molecule
+
+:returns: dictionary of chain molecule selections
+:rtype: dictionary
+"""
+def create_chain_molecules(pdbfile, ligand_selection):
+    mol = scoria.Molecule()
+    mol.load_pdb_into(pdbfile)
+    #get_ligand_selections
+    select_dict = mol.selections_of_chains()
+    chain_mol = []
+    for chain in select_dict.keys():
+        selection = select_dict[chain]
+		#append ligand selection to this
+        selection = np.append(selection, ligand_selection)
+        new_mol = mol.get_molecule_from_selection(selection)
+        chain_mol.append(copy.deepcopy(new_mol))
+    return(chain_mol)
+
+
+#Gets the Amino Acid Sequence of each protein
+#Any questions, ask Dr. Durrant
+"""
+Given the structure object of a molecule, obtains 
+the sequence of amino acid
+
+:params str filename: strucutre of protein
+
+:returns: amino acid sequence
+:rtype: string
+"""
+def get_seq(struc):
+    ppb = PPBuilder()
+    sequence = ""
+    for pp in ppb.build_peptides(struc):
+        sequence = sequence+pp.get_sequence()
+        return sequence
+
+#Function for aligning query protein with BLAST results
+#Any questions, ask Dr. Durrant
+"""
+Given the structures the query protein and matched protein as well
+as the name of the query protein, structurally aligns the matched protein
+to the query protein and saves the to teh directory moved
+
+:params structure template: strucutre of query protein 
+        structure moving: strucutre of matched protein
+        str query: name of query protein
+
+:returns: null
+:rtype: null
+"""
+def align_proteins(template, moving, query):
+	cwd = os.getcwd()
+	fastaFileName = cwd+'/Alignment_'+query+'/pdb'+moving+'.fasta'
+	PDB_File_Name = cwd+'/Moved_'+query+'/pdb'+'moved'+moving+'.pdb'
+	alnFileName = cwd+'/Alignment_'+query+'/pdb'+moving+'.aln'
+	# Load in the moving PDB
+	moving = PDBParser().get_structure(moving, cwd+'/Chains_'+query+'/pdb'+moving.lower()+".pdb")
+	
+	seq1 = get_seq(template)
+	seq2 = get_seq(moving)
+	
+	# Make a fake fasta file
+	header_line = ">protein1"
+	header_line2 = ">protein2"
+	t = open(fastaFileName,'w')
+	t.write(header_line + "\n")
+	t.write(str(seq1) + "\n")
+	t.write("\n")
+	t.write(header_line2 + "\n")
+	t.write(str(seq2) + "\n")
+	t.write("\n")
+	t.close()
+
+	# Use clustal to align the two sequences
+	#info_path = os.path.dirname(os.path.abspath(__file__))
+	#clustalw2_exe =  info_path + "/clustalw-2.1-linux-x86_64-libcppstatic/clustalw2"
+	clustalw2_exe = r"C:\Program Files (x86)\ClustalW2\clustalw2.exe"
+	clustalw_cline = ClustalwCommandline(clustalw2_exe, infile=fastaFileName)
+	stdout, stderr = clustalw_cline()
+	
+	# Load the alignment file into an alignment object
+	alignment = AlignIO.read(alnFileName, "clustal")
+	
+	try:
+		# Now map that alignment onto the two proteins
+		# Tells us which residue in one structure is equivalent to the residue in the other structure
+		# Based on the sequence alignment you performed with clustalw2.
+		a = StructureAlignment(alignment, template, moving)
+		maps = a.get_maps()
+		# Now make lists, where entry x on each list corresponds to equivalent CA atoms
+		ref_atoms = []
+		moving_atoms = []
+		for template_res in maps[0].keys():
+			moving_res = maps[0][template_res]
+			if not template_res is None and not moving_res is None:
+				ref_atoms.append(template_res["CA"])
+				moving_atoms.append(moving_res["CA"])
+		# Now superimpose the structures by aligning those CAs
+		super_imposer = Superimposer()
+		super_imposer.set_atoms(ref_atoms, moving_atoms)
+		super_imposer.apply(moving.get_atoms())
+
+		# Save the aligned file
+		io = PDBIO()
+		io.set_structure(moving) 
+		io.save(PDB_File_Name)
+	except:
+		print('Error. Too many gaps in the structure')
